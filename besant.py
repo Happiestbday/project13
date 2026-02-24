e8fac3175c74f5856fa9b0e8376f2dd078cea7a8
Happiestbday <106513813+Happiestbday@users.noreply.github.com>
2026-02-24 14:28:06 +0530

    Update besant.py

--- besant.py
+++ besant.py
@@ -1,3 +1,4 @@
 print("Hellow all in Devops Beasnt class")
 print("Hellow all in Devops Beasnt class again")
 print("Hellow all in Devops Beasnt class again in new repo feb")
+print("Hellow all in Devops Beasnt class again in new repo feb for pull from remote to local")
